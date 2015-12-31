9ce2a26877333b55f87e1d7e6ea6fd7a08037b35
serfer89 <sergpavlov89@gmail.com>
2015-12-31 11:37:04 +0200

    Update php.php

--- php.php
+++ php.php
@@ -14,7 +14,12 @@ echo "<div>
 	$full_name="/".$a->path."/".$a->file_name;
 	echo "<div ><div id=\"left\" class=\"left_slide\" onclick=\"left(".$a->left.")\"></div>
 	<img id=\"image_div\" class=\"port_img\" src=\"".$full_name."\" alt=\"".$a->file_name."\" onclick=\"right(".$a->right.")\">
-	<div style=\"background-color: rgb(149, 242, 143); width: 60%; margin: 0px auto;\">".$a->capture."</div></div></div>";
+	<div style=\"background-color: rgb(149, 242, 143); width: 60%; margin: 0px auto;\">".$a->capture."</div></div></div>
+	<script>
+	var h=$(window).height();
+h=h-150;
+			$('#image_div').css('max-height',h);
+	</script>";
 	 
 	
 	$a=null;
@@ -24,39 +29,53 @@ echo "<div>
 	
 	
 		$category=$_POST['category'];
-$a=new port_class();
+		$a=new port_class();
 	
-	$a->get_img("category", $category);
+		$a->get_img("category", $category);
 
 	
 	break;
 	
 	
 	case "selected_last_num":
-	$id=$_POST['id'];
+		$id=$_POST['id'];
+
+
 	
-	$sql="SELECT `url` FROM `port_menu` WHERE `id` LIKE '".$id."'";
+		$sql="SELECT `url` FROM `port_menu` WHERE `id` LIKE '".$id."'";
 	
-	$result = $mysqli->query($sql);
-if ($result->num_rows > 0) {
+		$result = $mysqli->query($sql);
+
+		if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
+   		 while($row = $result->fetch_assoc()) {
 		$name=$row['url'];
 
-	}//while
+		}//while
 
 
-}
+		}
 	
-	
-	$l=new port_menu();
-	$l->last_num($name);
-	echo $l->number;
+			$sql_sub="SELECT `id` FROM `portfolio` WHERE `sub` != '' AND `category` LIKE '".$name."'";
+			$result_sub = $mysqli->query($sql_sub);
+
+				if ($result_sub->num_rows > 0)
+					{
+					$result_get_sub = $mysqli->query("SELECT * FROM `sub_cat` WHERE `category` LIKE '".$name."'");	
+					echo "<select name=\"get_sub\"><option>...</option>";
+					   		 while($row = $result_get_sub->fetch_assoc()) {
+							echo "<option value=".$row['id'].">".$row['name']."</option>";
+ 							}	
+					echo "</select>";
+					}
+		$l=new port_menu();
+		$l->last_num($name, $sub);
+		echo $l->number;
 	
 	break;
 	
 	
-		case "edit_one_photo":
+	case "edit_one_photo":
 		
 		$id=$_POST['id'];
 		$img=new port_class();
@@ -67,8 +86,8 @@ if ($result->num_rows > 0) {
 		<img class=\"port_img\" src=\"".$full_name."\" alt=\"".$img->file_name."\">
 		<textarea style=\"width:80%\" id=\"upt_capture\">".$img->capture."</textarea>
 		<input type=\"hidden\" name=\"upd_id\" value=\"".$id."\">
-		<input type=\"button\" value=\"upd\" onclick=\"update()\">
-		<input type=\"button\" value=\"del\" onclick=\"delete_it()\">
+		<input type=\"button\" value=\"Upd\" onclick=\"update()\">
+		<input type=\"button\" value=\"Del\" onclick=\"delete_it()\">
 		</div>";
 		break;
 	case "upd_port_photo":
@@ -79,33 +98,33 @@ if ($result->num_rows > 0) {
 		$sql="UPDATE `u802398134_magic`.`portfolio` SET `capture` = '".$capture."' WHERE `portfolio`.`id` ='".$id."';";
 		
 		if ($mysqli->query($sql) === TRUE) {
-    echo "Запись успешно обновлена!";
-	$_POST['edit']=null;
-	$mysqli->close();
-} else 
-{
-    echo "Error: " . $sql . "<br>" . $conn->mysqli_error;
-	$mysqli->close();
-}
+    		echo "Запись успешно обновлена!";
+		$_POST['edit']=null;
+		$mysqli->close();
+			} else 
+				{
+  				  echo "Error: " . $sql . "<br>" . $conn->mysqli_error;
+					$mysqli->close();
+				}
 		
 		
 	break;
 
-case "del_port_photo":
-$id= $_POST['id'];
-$sql="DELETE FROM `portfolio` WHERE `id` = '".$id."'";
-echo $sql;
+	case "del_port_photo":
+		$id= $_POST['id'];
+		$sql="DELETE FROM `portfolio` WHERE `id` = '".$id."'";
+		echo $sql;
 		if ($mysqli->query($sql) === TRUE) {
-    echo "Запись успешно удалена!";
-	$_POST['edit']=null;
-	$mysqli->close();
-} else 
-{
-    echo "Error: " . $sql . "<br>" . $conn->mysqli_error;
-	$mysqli->close();
-}
+ 		  echo "Запись успешно удалена!";
+			$_POST['edit']=null;
+			$mysqli->close();
+			} else 
+				{
+  				  echo "Error: " . $sql . "<br>" . $conn->mysqli_error;
+					$mysqli->close();
+				}
 
-break;
+	break;
 	
 	case "edit_port_text":
 	$title=$_POST['title'];
@@ -114,9 +133,9 @@ break;
 	echo "
 	
 	
-<script type=\"text/javascript\">
-   tinymce.PluginManager.load('moxiecut', '/sys/js/tinymce/plugins/moxiecut/plugin.min.js');
-    tinymce.init({
+	<script type=\"text/javascript\">
+  	 tinymce.PluginManager.load('moxiecut', '/sys/js/tinymce/plugins/moxiecut/plugin.min.js');
+  	  tinymce.init({
         selector: \"textarea\",
 		keep_styles: false,
 		extended_valid_elements : 'h3[onclick|class|id]',
@@ -129,7 +148,7 @@ break;
     	toolbar: \"code emoticons | styleselect | bold italic | alignleft aligncenter alignright alignjustify | forecolor backcolor | link insertfile image\",
       
     });
-</script>	
+	</script>	
 	
 	
 <form method=\"post\" action=\"../sys/action.php\" enctype='multipart/form-data'>
